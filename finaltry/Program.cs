e6c9dac55101ac577e180dc7fbc301520305e5bd
TarikD0 <tarikdedekorkut@gmail.com>
2024-05-13 12:26:13 +0300

    Testing

--- finaltry/Program.cs
+++ finaltry/Program.cs
@@ -11,7 +11,7 @@ namespace finaltry
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        [STAThread] 
         static void Main()
         {
             Application.EnableVisualStyles();
